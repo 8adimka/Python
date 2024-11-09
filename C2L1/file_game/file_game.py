d3701c385526d4dce25deac2ae1c42b0ed1d4b63
Grimsy <m8adimka@gmail.com>
2024-11-09 12:33:14 +0100

    dir rename

--- C2L1/file_game/file_game.py
+++ C2L1/file_game/file_game.py
@@ -4,7 +4,7 @@ player_name = input ('Введите ваше имя:\n->')
 print (f'Пользователь: {player_name}')
 score = 0
 
-with open (r'C:\Users\m8adi\Desktop\Python\Test_2\files\words.txt', 'r') as file:
+with open (r'C:\Users\m8adi\Desktop\Python\C2L1\file_game\words.txt', 'r') as file:
     for line in file:
         secret_word = list (line.strip())
         random.shuffle (secret_word)
@@ -16,7 +16,7 @@ with open (r'C:\Users\m8adi\Desktop\Python\Test_2\files\words.txt', 'r') as file
         else:
             print (f'Неверно! Верный ответ: {line.strip()}')
 
-with open (r'C:\Users\m8adi\Desktop\Python\Test_2\files\history.txt', 'a+', encoding='utf-8') as file:
+with open (r'C:\Users\m8adi\Desktop\Python\C2L1\file_game\history.txt', 'a+', encoding='utf-8') as file:
     file.write (f'{player_name}   {score}\n')
     file.seek(0)
     total = [line.strip() for line in file]
