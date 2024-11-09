09abbbcd4a24b9b6809039c4a8e413e37c4bbd86
Grimsy <m8adimka@gmail.com>
2024-11-09 12:33:14 +0100

    dir rename

--- C3L2/C3L2Test1.py
+++ C3L2/C3L2Test1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os, random, json
 app = Flask(__name__)
 
 @app.route('/card')
@@ -14,8 +15,9 @@ def user_card():
 
 
 
-@app.route('/city')
-def cites ():
+@app.route('/city/')
+@app.route('/city/<int:numb>')
+def cites (numb = None):
     c_dict = {
         1: "Самара",
         2: "Краснодар",
@@ -23,7 +25,8 @@ def cites ():
         4: "Новосибирск",
         5: "Вышгород"
         }
-    return render_template('city.html', c_dict = c_dict)
+    results = numb
+    return render_template('city.html', c_dict = c_dict, results=results)
 
 
 
@@ -56,7 +59,7 @@ def user_form():
 
 
 DATA_BASE = {'v': 'Killer1911!', 'm8adimka': 'Killer1911!'}
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/pass/', methods=['GET', 'POST'])
 def user_password():
     username = request.form.get('username', '').lower()
     password = request.form.get('password', '')
@@ -82,6 +85,83 @@ def user_password():
 #             results = False  # Если данные неверны, указываем доступ запрещен
 
 #     return render_template('user_password.html', results=results)
+
+
+
+UPLOAD_FOLDER = r'C:\Users\m8adi\Desktop\Python\C3L2\uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Создаем папку uploads, если её нет
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/load/', methods=['GET', 'POST'])
+def upload_file():
+    message = ""
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            message = 'Файл не найден'
+        else:
+            file = request.files['file']
+            if file.filename == '':
+                message = 'Файл не выбран'
+            else:
+                filename_list = file.filename.split('.')
+                filename = f'{random.randint(100000, 999999)}.{filename_list[-1]}'                
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+                message = f'Файл {filename} успешно загружен в {file_path}<br>'
+
+                if filename_list[-1] in ['png', 'jpg', 'tiff', 'svg']:
+                    message += 'и это картинка'
+                elif filename_list[-1] in ['txt', 'doc', 'rtf']:
+                    message += 'и это текст'
+                else:
+                    message += 'возможно это PDF или я не знаю такого формата'
+    return render_template('file_load.html', message=message)
+
+
+@app.route('/', methods=['GET', "POST"])
+def upload_file2():
+    message = ""
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            message = 'Файл не найден'
+        else:
+            file = request.files['file']
+            if file.filename == '':
+                message = 'Файл не выбран'
+            else:
+                UPLOAD_FOLDER = r'C:\Users\m8adi\Desktop\Python\C3L2'              
+                folder_path = os.path.join(UPLOAD_FOLDER, request.form.get('folder', ''))
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+                file_path = os.path.join(folder_path, file.filename)
+                file.save(file_path)
+                message = f'Файл {file.filename} успешно загружен в {file_path}<br>'
+    return render_template('file_load2.html', message=message)
+
+
+@app.route('/json/', methods=['GET', 'POST'])
+def upload_file3():
+    message = ""
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            message = 'Файл не найден'
+        else:
+            file = request.files['file']
+            if file.filename == '':
+                message = 'Файл не выбран'
+            else:
+                filename_list = file.filename.split('.')               
+                if filename_list[-1].lower() == 'json':
+                    data = json.load(file)
+                    print(data)  # Выводим данные в консоль
+                    message = f'JSON содержимое: {data}'
+                else:
+                    message = 'это не json'
+    return render_template('file_load3.html', message=message)
+
 
 if __name__ == "__main__":
     app.run()
